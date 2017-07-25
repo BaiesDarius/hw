3c3689e98542e08bafa42bdd791ccb9f7e9ea18a
DESKTOP-M7QB9IB\dareu <darius_darius91@yahoo.com>
2017-07-25 15:52:40 +0300

    1

---
+++ SiteEvents/SiteEvents/Models/Event.cs
@@ -0,0 +1,29 @@
+﻿using SiteEvents.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RequestWorkflow.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Event Name")]
+        public string EventName { get; set; }
+
+        [Required]
+        [DisplayName("Location")]
+        public string Location { get; set; }
+
+        [Required]
+        [DisplayName("Event Date")]
+        [DataType(DataType.Date)]
+        public DateTime EventDate { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+
+    }
+}
